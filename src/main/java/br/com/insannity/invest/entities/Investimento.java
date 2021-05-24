9a93bac551e1d8c67fafb81a3e5bf5b1813d0cad
insannity <32269793+insanniity@users.noreply.github.com>
2021-05-24 13:40:39 -0300

    Update Investimento.java

--- src/main/java/br/com/insannity/invest/entities/Investimento.java
+++ src/main/java/br/com/insannity/invest/entities/Investimento.java
@@ -3,17 +3,24 @@ package br.com.insannity.invest.entities;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 @Entity
 public class Investimento {
 	
-	@Id
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
 	private String codigoAtivo;
@@ -22,7 +29,12 @@ public class Investimento {
 	
 	private Integer quantidadeCotas;
 	
-	private LocalDate data;
+	private LocalDate data;	
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_categoria")
+	private Categoria categoria;
+	
 	
 	
 }
