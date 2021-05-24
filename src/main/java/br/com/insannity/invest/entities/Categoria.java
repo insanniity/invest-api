1ad79314626d666ed0d5a3e3eb80df4e0ec8b46e
insannity <32269793+insanniity@users.noreply.github.com>
2021-05-24 13:40:36 -0300

    Create Categoria.java

---
+++ src/main/java/br/com/insannity/invest/entities/Categoria.java
@@ -0,0 +1,26 @@
+package br.com.insannity.invest.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+public class Categoria {
+	
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	private String nome;
+	
+	private String Descricao;
+	
+}
